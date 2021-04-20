cfddc8984ddffcb6eede632eaf11dbe48ae2488a
Barry Chocolate <barrychocolate@yahoo.co.uk>
2021-04-20 16:22:33 +0100

    Added books data in sqlite format. Updated api to return books based on id, author, year of publishing Added 404 error page Added requirements document to allow the loading of packages into an enviroment.

--- api.py
+++ api.py
@@ -1,75 +1,98 @@
 import flask
 from flask import request, jsonify
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city '
-                       'Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Distant Reading Archive</h1>
 <p>A prototype API for distant reading of science fiction novels.</p>
+Base Url : http://127.0.0.1:5000/<Br>
+<Br>
 <h2>All Books</h2>
-This will provide the three test books in our catalogue, in JSON.<br>
+Return all 67 books in our catalogue, in JSON.<br>
 <br>
-<b>Path:</b> api/v1/resources/books/all<br>
-<b>Parameters:</b><br>
-<b>Example:</b> http://127.0.0.1:5000/api/v1/resources/books/all<br>
+<b>Path : </b>api/v1/resources/books/all<br>
+<b>Parameters :</b><br>
+<b>Example : </b><br>
+<a href="http://127.0.0.1:5000/api/v1/resources/books/all" target="_blank">http://127.0.0.1:5000/api/v1/resources/books/all</a><br>
 <br>
 <h2>Return a specific book</h2>
-This will provide the three test books in our catalogue, in JSON.<br>
+Select the books you want by supplying id, year of publication or author.<br>
 <br>
 <b>Path:</b> /api/v1/resources/books<br>
-<b>Parameters:</b> id (used to provide the id of the book you want to return)<br>
-<b>Example:</b> http://127.0.0.1:5000/api/v1/resources/books?id=1 (this will return the book with id 1)'''
+<b>Parameters:</b><br>
+    id : int (id of the book you want to return)<br>
+    published : int (year of publication)
+    author : str (Author's name) 
+<br>
+<br>
+Return all books by Connie Willis<br>
+<a href="http://127.0.0.1:5000/api/v1/resources/books?author=Connie+Willis" target="_blank">http://127.0.0.1:5000/api/v1/resources/books?author=Connie+Willis</a><br>
+<br>
+Return all books by Connie Willis, published in 1999<br>
+<a href="http://127.0.0.1:5000/api/v1/resources/books?author=Connie+Willis&published=1999" target="_blank">http://127.0.0.1:5000/api/v1/resources/books?author=Connie+Willis&published=1999</a><br>
+<br>
+Return all books published in 2010<br>
+<a href="http://127.0.0.1:5000/api/v1/resources/books?published=2010" target="_blank">http://127.0.0.1:5000/api/v1/resources/books?published=2010</a>'''
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
-    return jsonify(books)
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_books = cur.execute('SELECT * FROM books;').fetchall()
+
+    return jsonify(all_books)
 
 
 @app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
+def api_filter():
+    query_parameters = request.args
 
-    # Create an empty list for our results
-    results = []
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
 
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+    if not (id or published or author):
+        return page_not_found(404)
+
+    query = query[:-4] + ';'
+    print(query)
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
     return jsonify(results)
 
 
